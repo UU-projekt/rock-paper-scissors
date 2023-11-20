3bc548cd89efea36d57dd4e7642fe1f274fbf81b
ffamilyfriendly <jonathan.hermin.3538@student.uu.se>
2023-11-20 15:12:55 +0100

    Add project files.

---
+++ rockpaperscissors/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockpaperscissors
+{
+    internal class Menu
+    {
+        public static void Intro()
+        {
+            Console.WriteLine("Vill du spela mot en annan spelare?");
+            string? alternativ = Console.ReadLine();
+
+            if (alternativ == "ja" || alternativ == "Ja" || alternativ == "JA")
+            {
+            }
+        }
+    }
+}
---
+++ rockpaperscissors/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using rockpaperscissors;
+
+Console.WriteLine("Hello, World!");
+
+Menu.Intro();
